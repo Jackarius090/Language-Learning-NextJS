d602ca6f15e5645cc35282f1e88099b5f9e3d304
Jack Dickinson <jackdickinson090@hotmail.com>
2025-02-20 19:11:48 +0100

    deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
-  return <div className="text-center">Hey</div>;
+  return (
+    <>
+      <h1 className="text-center mt-10 text-4xl">Language Learning</h1>
+      <div className="flex gap-10 m-32">
+        <Textarea />
+        <div>Translated text</div>
+      </div>
+    </>
+  );
 }
