6ba8267775782c22a3188de3a976ee186a2778c7
Jack Dickinson <jackdickinson090@hotmail.com>
2025-05-25 21:09:57 +0200

    number game progress

--- src/app/numbergame/page.tsx
+++ src/app/numbergame/page.tsx
@@ -1,10 +1,32 @@
+"use client";
 import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
+import { useState } from "react";
+
 export default function NumberGame() {
+  const [number, setNumber] = useState(0);
+
+  function numberGenerator() {
+    const num = Math.floor(Math.random() * 100);
+    setNumber(num);
+  }
   return (
-    <div>
-      <h1>Number Game!</h1>
-      <Link href={"/"}>Return to main page</Link>
-      <section>Number game here:</section>
+    <div className="min-h-screen size-96 mx-auto flex flex-col justify-center gap-10">
+      <h1 className="text-center text-4xl text-nowrap">Number Game!</h1>
+      <div className="flex gap-4 justify-center">
+        <Button variant="outline" className="h-10">
+          <Link href={"/"}>Return to main page</Link>
+        </Button>
+        <ModeToggle />
+      </div>
+
+      <section className="size-80 bg-slate-700">
+        <Button onClick={numberGenerator}>Get Number</Button>
+        {number}
+        <Input placeholder="type number here"></Input>
+      </section>
     </div>
   );
 }
